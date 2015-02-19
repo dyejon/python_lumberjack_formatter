50a9fe65915b68087cc2c523b1b55eb7b2b335ea
Jonathan Dye <jdye@us.ibm.com>
2015-02-19 08:24:14 -0700

    add urls expand tabs

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ import os
 from setuptools import setup, find_packages, findall
 
 def get_version():
-	with open(os.path.join(thisdir, 'VERSION'), 'r') as fh:
-		return fh.readline().strip()
+    with open(os.path.join(thisdir, 'VERSION'), 'r') as fh:
+        return fh.readline().strip()
 
 def get_requires(filename='requirements.txt'):
-	requires = []
-	with open(os.path.join(thisdir, filename), 'r') as fh:
-		for line in fh.readlines():
-			requires += [ line.strip() ]
-	return requires
+    requires = []
+    with open(os.path.join(thisdir, filename), 'r') as fh:
+        for line in fh.readlines():
+            requires += [ line.strip() ]
+    return requires
 
 thisdir = os.path.dirname(os.path.realpath(__file__))
 
@@ -28,16 +28,18 @@ ALL_TEST_REQUIRES = get_requires('test_requirements.txt')
 ALL_SETUP_REQUIRES = get_requires('setup_requirements.txt')
 
 setup(
-	name='lumberjack_formatter',
-	version=VERSION,
-	description='Python logging framework JSON formatter for use with the lumberjack transport (logstash-forwarder)',
-	author='IBM',
-	author_email='jdye@us.ibm.com',
-	packages=ALL_PACKAGES,
-	provides=ALL_PACKAGES,
-	scripts=SCRIPTS,
-	include_package_data=True,
-	setup_requires=ALL_SETUP_REQUIRES,
+    name='lumberjack_formatter',
+    version=VERSION,
+    description='Python logging framework JSON formatter for use with the lumberjack transport (logstash-forwarder)',
+    author='IBM',
+    author_email='jdye@us.ibm.com',
+    url='https://github.com/dyejon/python_lumberjack_formatter',
+    download_url='https://github.com/dyejon/python_lumberjack_formatter/tarball/%s' % VERSION,
+    packages=ALL_PACKAGES,
+    provides=ALL_PACKAGES,
+    scripts=SCRIPTS,
+    include_package_data=True,
+    setup_requires=ALL_SETUP_REQUIRES,
     install_requires=ALL_INSTALL_REQUIRES,
     tests_require=ALL_TEST_REQUIRES,
     test_suite='nose.collector'
