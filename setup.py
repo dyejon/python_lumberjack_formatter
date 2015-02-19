b0cd5abd1b2e952e8102701abe6f4e48cd0b03e7
Jonathan Dye <jdye@us.ibm.com>
2015-02-19 07:54:39 -0700

    first release

--- setup.py
+++ setup.py
@@ -6,22 +6,26 @@ def get_version():
 	with open(os.path.join(thisdir, 'VERSION'), 'r') as fh:
 		return fh.readline().strip()
 
-def get_requires():
+def get_requires(filename='requirements.txt'):
 	requires = []
-	with open(os.path.join(thisdir, 'requirements.txt'), 'r') as fh:
+	with open(os.path.join(thisdir, filename), 'r') as fh:
 		for line in fh.readlines():
 			requires += [ line.strip() ]
 	return requires
 
-SCRIPTS = findall('bin')
-
 thisdir = os.path.dirname(os.path.realpath(__file__))
 
+# parse version number from text file
 VERSION = get_version()
 
+# find packages and binaries
 ALL_PACKAGES = find_packages()
+SCRIPTS = findall('bin')
 
+# parse requirements from text files
 ALL_INSTALL_REQUIRES = get_requires()
+ALL_TEST_REQUIRES = get_requires('test_requirements.txt')
+ALL_SETUP_REQUIRES = get_requires('setup_requirements.txt')
 
 setup(
 	name='lumberjack_formatter',
@@ -33,9 +37,10 @@ setup(
 	provides=ALL_PACKAGES,
 	scripts=SCRIPTS,
 	include_package_data=True,
-	setup_requires=(
-		'setuptools>=11.0,<13.0'
-	),
-	install_requires=ALL_INSTALL_REQUIRES,
+	setup_requires=ALL_SETUP_REQUIRES,
+    install_requires=ALL_INSTALL_REQUIRES,
+    tests_require=ALL_TEST_REQUIRES,
+    test_suite='nose.collector'
 )
 
+# vim: set ts=4 sw=4 expandtab:
